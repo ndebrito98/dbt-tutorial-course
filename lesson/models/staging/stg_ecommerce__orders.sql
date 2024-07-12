636f2141f9a8ad78f8b88912361ff6bec3df0c35
ndebrito98 <ndebrito@bamboohealth.com>
2024-07-12 08:34:05 -0700

    49. getting rest of staging files set up

--- lesson/models/staging/stg_ecommerce__orders.sql
+++ lesson/models/staging/stg_ecommerce__orders.sql
@@ -5,13 +5,18 @@ WITH source AS (
 )
 
 SELECT
-        order_id,
-        user_id,
-        status,
-        created_at,
-        returned_at,
-        shipped_at,
-        delivered_at,
-        num_of_item
+    -- IDs
+	order_id,
+	user_id,
+
+	-- Timestamps
+	created_at,
+	returned_at,
+	delivered_at,
+	shipped_at,
+
+	-- Other columns
+	status,
+	num_of_item AS num_items_ordered
 
 FROM source
